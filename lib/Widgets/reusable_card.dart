b8c3ae82225447258a07d39a395cd995148d1cdd
Numan <nomanuddin2012@gmail.com>
2022-07-26 15:36:35 +0500

    changes

--- lib/Widgets/reusable_card.dart
+++ lib/Widgets/reusable_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-
   ReusableCard({@required this.color, this.childWidget, this.onPress});
+
   final Color color;
   final Widget childWidget;
   final Function onPress;
@@ -14,8 +14,8 @@ class ReusableCard extends StatelessWidget {
       child: Container(
         child: childWidget,
         margin: EdgeInsets.all(15),
-        decoration:
-        BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10)),
         height: 200,
         width: 170,
       ),
